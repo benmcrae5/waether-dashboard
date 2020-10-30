a4da8804bc8c6009a7819fdb13c78b7836e5dfdf
Ben McRae <benmcrae5@yahoo.com>
2020-10-29 21:14:42 -0700

    WeatherDashboard v.03 - basic JS structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
+let formEl = $(".search-form")
+
 //create fetch function for today's forecast
-
+let getTodayForecast = function() {
+    console.log("Today's forecast");
+}
 //create fetch function for five-day forecast
-
+let getFiveDayForecast = function() {
+    console.log("5-day forecast");
+}
 //function for populating search results
+let runSearch = function(event) {
+    event.preventDefault();
+    let todayForecast = getTodayForecast();
+    let fiveDatForecast = getFiveDayForecast();
+}
 
 //event handler for submit on form
+formEl.on("submit", runSearch);
