581edf9f47f0619af102407c45b2de8eee8f6a0c
Ben McRae <benmcrae5@yahoo.com>
2020-10-31 20:22:58 -0700

    WD v3.0 - final Version, added input verification

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,17 @@ let cityCoord = {"lon": 0, "lat": 0};
 let getCityCoordinates = function (city) {
     let apiUrlForCoord = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
     fetch (apiUrlForCoord)
-        .then(response => response.json())
+        .then(function (response) {
+            if (response.ok) {
+                return response.json()
+            }
+        })
         .then(function(object) {
-            cityCoord["lon"] = object.coord.lon;
-            cityCoord["lat"] = object.coord.lat;
-            getTodayWeather(cityCoord, city);
+            if (object) {
+                cityCoord["lon"] = object.coord.lon;
+                cityCoord["lat"] = object.coord.lat;
+                getTodayWeather(cityCoord, city);
+            }
         })
 }
 
@@ -45,26 +51,32 @@ let setWeatherIcon = function(condition) {
     return icon;
 }
 
+//uses API info to create dynamic HTML display for today's forecast
 let displayTodayCard = function (todayObject, cityName) {
     let todayCard = $("<div>").addClass("card today-card");
     let weatherIcon = setWeatherIcon(todayObject.daily[0].weather[0].main);
-    $("<h4>").addClass("card-title")
+    $("<h4>")
+        .addClass("card-title")
         .html(cityName + " <img height='2em' width='2em' src='" + weatherIcon + "' alt='404'>")
         .appendTo(todayCard);
-    $("<h5>").addClass("card-title")
+    $("<h5>")
+        .addClass("card-title")
         .text(todayDate.format("l"))
         .appendTo(todayCard);
-    $("<p>").addClass("today-temp card-text")
+    $("<p>")
+        .addClass("today-temp card-text")
         .text("Temperature: " + todayObject.daily[0].temp.day + " °F")
         .appendTo(todayCard);
-    $("<p>").addClass("today-humidity card-text")
+    $("<p>")
+        .addClass("today-humidity card-text")
         .text("Humidity: " + todayObject.daily[0].humidity + "%")
         .appendTo(todayCard);
-    $("<p>").addClass("today-wind-speed card-text")
+    $("<p>")
+        .addClass("today-wind-speed card-text")
         .text("Wind Speed: " + todayObject.daily[0].wind_speed + " MPH")
         .appendTo(todayCard);
     let uviLevel = todayObject.daily[0].uvi;
-    let uviSpan = ""
+    let uviSpan;
     if (uviLevel >= 0 && uviLevel < 3) {
         uviSpan = '<span class="bg-success">' + uviLevel + '</span>';
     } else if (uviLevel >= 3 && uviLevel < 7) {
@@ -72,49 +84,45 @@ let displayTodayCard = function (todayObject, cityName) {
     } else {
         uviSpan = '<span class="bg-danger">' + uviLevel + '</span>';
     }
-    $("<p>").addClass("today-uvindex card-text")
+    $("<p>")
+        .addClass("today-uvindex card-text")
         .html("UV Index: " + uviSpan)
         .appendTo(todayCard);
 
     todayCard.appendTo(searchDivEl);
 }
 
+//uses API info to create dynamic HTML display for the 5-day forecast
 let display5DayCard = function (fiveDayObject, cityName) {
-    let fiveDayCard = $("<div>").addClass("five-day-display d-flex flex-column flex-lg-row justify-content-between");
-
+    let fiveDayCard = $("<div>").addClass("five-day-display");
+    $("<h3>")
+        .addClass("five-day-title")
+        .text("5-Day Forecast: ")
+        .appendTo(fiveDayCard);
+    let cardHolder = $("<div>")
+        .addClass("five-day-card-holder d-flex flex-column flex-lg-row justify-content-between")
+        .appendTo(fiveDayCard)
     for (let i = 0; i < 5; i++) {
         let dayCard = $("<div>").addClass("card today-card");
         let weatherIcon = setWeatherIcon(fiveDayObject.daily[i+1].weather[0].main);
 
-        $("<h5>").addClass("card-title")
+        $("<h5>")
+            .addClass("card-title")
             .text(todayDate.add(1, "days").format("l"))
             .appendTo(dayCard);
         $('<img>')
             .attr("src", weatherIcon)
             .appendTo(dayCard);
-        $("<p>").addClass("today-temp card-text")
+        $("<p>")
+            .addClass("today-temp card-text")
             .text("Temp: " + fiveDayObject.daily[i + 1].temp.day + " °F")
             .appendTo(dayCard);
-        $("<p>").addClass("today-humidity card-text")
+        $("<p>")
+            .addClass("today-humidity card-text")
             .text("Humidity: " + fiveDayObject.daily[i + 1].humidity + "%")
             .appendTo(dayCard);
-        /*$("<p>").addClass("today-wind-speed card-text")
-            .text("Wind: " + fiveDayObject.daily[i + 1].wind_speed + " MPH")
-            .appendTo(dayCard);
-        let uviLevel = fiveDayObject.daily[i + 1].uvi;
-        let uviSpan = ""
-        if (uviLevel >= 0 && uviLevel < 3) {
-            uviSpan = '<span class="bg-success">' + uviLevel + '</span>';
-        } else if (uviLevel >= 3 && uviLevel < 7) {
-            uviSpan = '<span class="bg-warning">' + uviLevel + '</span>';
-        } else {
-            uviSpan = '<span class="bg-danger">' + uviLevel + '</span>';
-        }
-        $("<p>").addClass("today-uvindex card-text")
-            .html("UV Index: " + uviSpan)
-            .appendTo(dayCard);
-        */
-        dayCard.appendTo(fiveDayCard);
+
+        dayCard.appendTo(cardHolder);
     }
 
     fiveDayCard.appendTo(searchDivEl);
@@ -133,72 +141,57 @@ let getTodayWeather = function(cityCoord, city) {
         });
 }
 
-//create fetch function for today's forecast
-let getTodayForecast = function(city) {
-    getCityCoordinates(city);
-
-    apiUrl = "http://api.openweathermap.org/data/2.5/onecall?lat=" + cityCoord["lat"] + "&lon=" + cityCoord["lon"] + "&units=imperial&appid=" + apiKey;
-
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(obj => console.log(obj));
-    //YOU ARE HERE!!!
-}
-
 //create fetch function for five-day forecast
 let getFiveDayForecast = function(apiUrl, city) {
     apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial";
     let fiveDayData = fetch(apiUrl).then(response => response.json())
-    console.log(fiveDayData);
 }
 
-//put search history in local storage
+//update the search history variable and UI, then put search history in local storage
 let updateSearchHistory = function(city, history) {
+    console.log("TEST6")
+
     if (!history.includes(city)) {
         history.push(city);
-        $("<p>").addClass("city-search " + city).text(city).appendTo(".search-history");
+        $("<p>")
+            .addClass("city-search " + city)
+            .text(city)
+            .appendTo(".search-history");
         localStorage.setItem("WeatherDashboard", JSON.stringify(history));
     }
-}
-
-//NOT NEEDED!! FOR TESTING ONLY!!!
-let testAPIData = function(apiUrl, city) {
-    apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
-    let todayData = fetch(apiUrl)
-        .then(response => response.json())
-        .then(x => console.log(x));
 }
 
 //function for populating search results
 let runSearch = function(event) {
     event.preventDefault();
+
     $(".search-results").empty();
 
-    console.log(event.type);
-    console.log(event.target);
-    let cityName = $("#city-name").val();
 
+    let cityName = $("#city-name").val();
     if (event.type === "click") {
         cityName = event.target.textContent;
     }
-    
-    getCityCoordinates(cityName);
-    updateSearchHistory(cityName, searchHistory);
+
+    let initiate = getCityCoordinates(cityName);
+
+
+    if (initiate) {
+        console.log("TEST: SAVE")
+
+        updateSearchHistory(cityName, searchHistory);
+    }
 }
 
 let populateSearchHistory = function() {
     for (let i = 0; i < searchHistory.length; i++) {
-        $("<p>").addClass("city-search " + searchHistory[i]).text(searchHistory[i]).appendTo(".search-history")
+        $("<p>")
+            .addClass("city-search " + searchHistory[i]).text(searchHistory[i])
+            .appendTo(".search-history")
     }
 }
 
 populateSearchHistory();
-
-let testHandler = function(event) {
-    let p = event.target
-    console.log(p, event.type);
-    console.log(p.textContent);
-}
 
 //event handler for submit on form
 formEl.on("submit", runSearch);
